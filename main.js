1add5750f17a0bd2beaf7ab1b0d6fc1137054ecd
grishbar <grishbar@mail.ru>
2019-06-03 03:02:11 +0500

    fix url to ru, delete unnecessary code lines

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // run code when search button is clicked
     $("#search").click(function () {
         let userInput = $("#user_input").val();         // gets the search input by the user
-        let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json&callback=?";
+        let url = "https://ru.wikipedia.org/w/api.php?action=opensearch&search=" + userInput + "&format=json&callback=?";
         if (userInput !== "") {
             $.ajax({
                 type: "GET",
@@ -10,13 +10,12 @@ $(document).ready(function () {
                 contentType: "application/json;charset=utf-8",
                 async: false,
                 dataType: "json",
-                success: function (data, textStatus, jqXHR) {
+                success: function (data) {
                     $('#output').html(''); //Перезаписать содержимое всех соответствующих элементов
                     for (let i = 0; i < data[1].length; i++) {
                         $('#output').prepend("<h2><a href=" + data[3][i] + ">" + data[1][i] + "</a></h2><p>" + data[2][i] + "</p></li>");
                         //data[3][i] - ссылка на статью  data[1][i] - заголловок статьи data[2][i] - сниппет
                     }
-                    $('#user_input').val('');
                 },
                 error: function (error) {
                     alert("error!");
